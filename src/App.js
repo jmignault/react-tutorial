f148649f947e6fd1dc1072f29dcf1edf2776c7a8
jmignault <john.mignault@gmail.com>
2018-09-05 15:44:50 -0400

    Build a form to create new characters

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
     state = {
-	characters: [
-	    {
-                'name': 'Charlie',
-                'job': 'Janitor'
-	    },
-	    {
-                'name': 'Mac',
-                'job': 'Bouncer'
-	    },
-	    {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-	    },
-	    {
-                'name': 'Dennis',
-                'job': 'Bartender'
-	    }
-	]
+	characters: []
     };
     
+    handleSubmit = character => {
+	this.setState({characters: [...this.state.characters, character]});
+    }
+
     removeCharacter = index => {
 	const { characters } = this.state;
 
@@ -39,6 +27,7 @@ class App extends Component {
 	      <Table
 		characterData={this.state.characters}
 		removeCharacter={this.removeCharacter}/>
+	      <Form handleSubmit={this.handleSubmit}/>
 	    </div>
 	);
     }
